ff5f22ea07a867529d58e73d0e2789d8ebc9aa0d
d c <carinodrex@gmail.com>
2022-01-31 16:35:42 +0800

    jan 31 monday

--- server/index.js
+++ server/index.js
@@ -8,10 +8,20 @@ const UserModel = require("./models/Users")
 const env = require("dotenv")
 env.config()
 const db_pw = process.env.MONGO_PW 
-const db_uname = process.env.MONGO_UNAME 
+const db_uname = process.env.MONGO_UNAME  
 
+const port = 3001
 
-mongoose.connect(`mongodb+srv://${db_uname}:${db_pw}@clsmern60.rwaa1.mongodb.net/mern60mins?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${db_uname}:${db_pw}@clsmern60.rwaa1.mongodb.net/mern60mins?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connection.on("error", err => {
+    console.log("connection error--------", err)
+    process.exit(0) //--close server on error
+})
+
+mongoose.connection.on("connected", (err, res) => {
+    console.log("mongoose is connected------------")
+})
 
 const cors = require("cors")
 
@@ -19,22 +29,36 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/getUsers", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if ( err ) return res.json(err)
+    // try {
+        // UserModel.find({username: "dyxx"}, (err, result) => {
 
-        res.json(result)
-    })
+        findstr = "a"
+
+        UserModel.find({ name: { $regex: findstr, $options: 'i' } })   
+            // .select('fullname name')
+            // .populate('fullname')
+            .exec( (err, result) => {
+                if ( err )  return res.status(502).json(err) 
+                return res.json(result)
+            })
+    //     const found = await UserModel.find({})
+
+    // } catch (error) {
+    //     console.log('try catch errrr', error);
+    //     res.json(error)
+    // }
 })
 
 app.post("/createUser", async (req, res) => {
     const param = req.body
     const newUser = new UserModel(param)
+
     const save_res = await newUser.save() 
 
     res.json( save_res )
 })
 
-app.listen(3001, () => {
-    console.log('server is running!');
+app.listen(port, () => {
+    console.log(`server is running! ${port}`);
 })
 
--- server/models/Users.js
+++ server/models/Users.js
@@ -15,6 +15,19 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
+
+// SAMPLE VIRTUALS
+UserSchema
+    .virtual('fullname')
+    .get(function () {
+        const fullname = this.name + ' @ ' + this.username
+        return fullname
+    })
+
+//In the Docs : Does not include Virtual fields by default
+UserSchema.set('toObject', { virtuals: true });
+UserSchema.set('toJSON', { virtuals: true });
+
 const UserModel = mongoose.model("users", UserSchema)
 
 module.exports = UserModel
